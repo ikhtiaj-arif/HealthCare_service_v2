31f154174b46d6d33cc4c4d0628402bdb1575039
ikhtiaj-arif <ikhtiaj.arif@gmail.com>
2026-08-29 18:36:53 +0600

    feat: payment pdf invoice with email

--- src/app/module/appointment/appointment.service.ts
+++ src/app/module/appointment/appointment.service.ts
@@ -10,7 +10,11 @@ import { RequestUser } from "../../middleware/checkAuth";
 import { AppError } from "../../utils/appError";
 import httpStatus from "http-status";
 import { IBookAppointmentPayload } from "./appoint.interface";
-import { isBefore, isSameDay } from "date-fns";
+import { addMinutes, isBefore, isSameDay } from "date-fns";
+import app from "../../../app";
+import { transporter } from "../../lib/nodemailer";
+import  PDFDocument  from "pdfkit";
+import { margins } from "pdfkit/js/page";
 
 const bookAppointment = async (
   payload: IBookAppointmentPayload,
@@ -375,14 +379,61 @@ const bookAppointmentCallback = async (query: Record<string, any>) => {
     const executedPaymentResult = await executedPayment.json();
 
     if (status === "success") {
+      // create serial number
+      const appointment = await prisma.appointment.findUnique({
+        where: {
+          id: executedPaymentResult.merchantInvoiceNumber,
+        },
+        include: {
+          schedule: true,
+          patient: true,
+		  doctor: true
+        },
+      });
+      if (!appointment)
+        throw new AppError(httpStatus.NOT_FOUND, "appointment not found");
+
+      // total slots = 3, available = 3
+      //(total - available) + 1
+      const alreadyBookedSlots =
+        appointment.schedule.totalSlots - appointment.schedule.availableSlots;
+      const serialNumber = alreadyBookedSlots + 1;
+
+      // 25 August => 3:00 PM - 4:00 PM
+      // 1st person joining time => startDateTime = 2026-08-25T15:00:00.436Z => 3:00 PM
+      // serial number (1) - 1 * 20 => 0 minutes
+
+      // 2nd person joining time => startDateTime = 2026-08-25T15:20:00.436Z => 3:00 PM
+      // serial number (2) - 1 * 20 => 20 minutes
+
+      // 3nd person joining time => startDateTime = 2026-08-25T15:40:00.436Z => 3:00 PM
+      // serial number (3) - 1 * 20 => 40 minutes
+
+      const joiningTime = addMinutes(
+        appointment.schedule.startDateTime,
+        (serialNumber - 1) * 20,
+      );
+
       await tx.appointment.update({
         where: {
           id: executedPaymentResult.merchantInvoiceNumber,
         },
         data: {
           status: AppointmentStatus.CONFIRMED,
+          joiningTime,
+          serialNumber,
         },
       });
+      const newAvailableSlots = appointment.schedule.availableSlots - 1;
+      await tx.schedule.update({
+        where: {
+          id: appointment.schedule.id,
+        },
+        data: {
+          availableSlots: newAvailableSlots,
+        },
+      });
+
       await tx.payment.update({
         where: {
           appointmentId: executedPaymentResult.merchantInvoiceNumber,
@@ -394,6 +445,62 @@ const bookAppointmentCallback = async (query: Record<string, any>) => {
           paidAt: executedPaymentResult.paymentExecuteTime,
           gatewayResponse: executedPaymentResult,
         },
+      });
+
+      //generate pdf
+      const pdfDocument = new PDFDocument({ margin: 50 });
+      const pdfChunks: Buffer[] = [];
+      pdfDocument.on("data", (chunk: Buffer) => {
+        pdfChunks.push(chunk);
+      });
+
+	  const pdfReadyPromise = new Promise<Buffer>((resolve) => {
+		pdfDocument.on("end", ()=>{
+			resolve(Buffer.concat(pdfChunks))
+		})
+	  })
+
+      pdfDocument.fontSize(20).text("Healthcare System", { align: "center" });
+      pdfDocument.fontSize(14).text("Appointment Invoice", { align: "center" });
+      pdfDocument.moveDown(2);
+      pdfDocument
+        .fontSize(12)
+        .text(`Patient Name: ${appointment.patient?.name}`);
+      pdfDocument.text(`Patient Email: ${appointment.patient?.email}`);
+      pdfDocument.moveDown();
+
+      pdfDocument.text(`Doctor Name: ${appointment?.doctor?.name}`);
+      pdfDocument.text(`Specialization: ${appointment.doctor?.specialization}`);
+      pdfDocument.moveDown();
+
+      pdfDocument.text(
+        `Appointment Date: ${appointment.schedule.startDateTime.toDateString()}`,
+      );
+      pdfDocument.text(`Your Joining Time: ${joiningTime.toString()}`);
+      pdfDocument.text(`Your Serial Number: ${serialNumber}`);
+      pdfDocument.text(`Meeting Link: ${appointment.schedule.meetingLink}`);
+      pdfDocument.moveDown();
+
+      pdfDocument.text(`Amount Paid: ${executedPaymentResult.amount} BDT`);
+      pdfDocument.text(`Payment Method: bKash`);
+      pdfDocument.text(`Transaction Id: ${executedPaymentResult.trxID}`);
+      pdfDocument.text(`Paid At: ${executedPaymentResult.paymentExecuteTime}`);
+
+      pdfDocument.end();
+
+	  const pdfBuffer = await pdfReadyPromise
+
+      await transporter.sendMail({
+        from: config.email_sender,
+        to: appointment.patient.email,
+        subject: "Your Appointment Invoice - Healthcare System",
+        text: "Thank you for booking an appointment. Please find your invoice attached.",
+		attachments: [
+			{
+				filename: "invoice.pdf",
+				content: pdfBuffer
+			}
+		]
       });
       return {
         redirectUrl: `${config.frontend_url}/dashboard/my-appointments?status=success`,
